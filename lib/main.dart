b1926981c39ce8f7a81149f160be90d5811e09b3
abdullah <em.wildan.es@gmail.com>
2023-02-19 13:09:23 +0700

    belajar navigasi dengan route Navigator.push

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/row/row_demo_page.dart';
+import 'package:learn_flutter/route/first_route.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+main() => runApp(const Application());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      // home: const LoginPage(),
-      home: const RowDemoPage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstRoute(),
     );
   }
 }
-
-class LoginPagePurple extends StatelessWidget {
-  const LoginPagePurple({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple.shade200,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 100, bottom: 8),
-              width: double.infinity,
-              child: const Text(
-                'Welcome,',
-                style: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(
-              width: double.infinity,
-              child: Text(
-                'Glade To See You!,',
-                style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/*class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}*/
---
+++ lib/route/second_route.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.chevron_left),
+        ),
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Kembali'),
+        ),
+      ),
+    );
+  }
+}
